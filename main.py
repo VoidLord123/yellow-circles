c412451909dd8f8c73a6810e2d1052ab9659aaab
Ivan <ivan12kalininn@gmail.com>
2023-11-16 12:03:31 +0300

    Edited to random color of circles

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import sys
 from PyQt5.QtCore import QPointF
 from PyQt5.QtGui import QImage, QColor, QPainter
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
 from random import randint
+from UI import Ui_MainWindow
 
 
-class MainWindow(QMainWindow):
+class MainWindow(Ui_MainWindow, QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.img = QImage(500, 500, QImage.Format_ARGB32)
         self.img.fill(QColor(0, 0, 0, 0))
         self.pushButton.clicked.connect(self.button_handler)
@@ -19,7 +19,7 @@ class MainWindow(QMainWindow):
         self.img = QImage(500, 500, QImage.Format_ARGB32)
         self.img.fill(QColor(0, 0, 0, 0))
         qp = QPainter(self.img)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         r = randint(50, 250)
         qp.drawEllipse(QPointF(250, 250), r, r)
         qp.end()
